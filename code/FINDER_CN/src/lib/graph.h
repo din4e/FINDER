f002dabc25ae82c30b1d2605c7050e377a7344c9
din4e <35624811+din4e@users.noreply.github.com>
2020-11-17 16:50:35 +0800

    update graph.h

--- code/FINDER_CN/src/lib/graph.h
+++ code/FINDER_CN/src/lib/graph.h
@@ -14,10 +14,9 @@ public:
     ~Graph();
     int num_nodes;
     int num_edges;
-    std::vector< std::vector< int > > adj_list;
-    std::vector< std::pair<int, int> > edge_list;
-    double getTwoRankNeighborsRatio(std::vector<int> covered);
-
+    std::vector< std::vector< int > > adj_list;                      // 临界矩阵
+    std::vector< std::pair<int, int> > edge_list;                    //
+    double getTwoRankNeighborsRatio(std::vector<int> covered);       // 
 };
 
 class GSet
@@ -25,14 +24,14 @@ class GSet
 public:
     GSet();
     ~GSet();
-    void InsertGraph(int gid, std::shared_ptr<Graph> graph);
-    std::shared_ptr<Graph> Sample();
-    std::shared_ptr<Graph> Get(int gid);
+    void InsertGraph(int gid, std::shared_ptr<Graph> graph);  // 插入一个Graph实例指针
+    std::shared_ptr<Graph> Sample();                          // 随机获取一个Graph实例指针
+    std::shared_ptr<Graph> Get(int gid);                      // 获得gid的Graph实例指针
     void Clear();
-    std::map<int, std::shared_ptr<Graph> > graph_pool;
+    std::map<int, std::shared_ptr<Graph> > graph_pool;        // id->Graph指针的映射表
 };
 
-extern GSet GSetTrain;
-extern GSet GSetTest;
+extern GSet GSetTrain;                                        // 训练集
+extern GSet GSetTest;                                         // 测试集
 
 #endif
